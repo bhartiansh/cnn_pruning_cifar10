e8973578bba569e9f743b7bbca234f67f5b5b166
bhartiansh <139680027+bhartiansh@users.noreply.github.com>
2025-04-16 23:21:46 +0530

    Update preprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,9 +1,11 @@
-# preprocessing.py
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-
 def load_cifar10_data(test_size=0.2):
-    (x, y), _ = tf.keras.datasets.cifar10.load_data()
-    x = x.astype('float32') / 255.0
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=test_size, stratify=y, random_state=42)
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train, x_val, y_train, y_val = train_test_split(
+        x_train, y_train, test_size=test_size, random_state=42, stratify=y_train
+    )
+
+    x_train = x_train.astype("float32") / 255.0
+    x_val = x_val.astype("float32") / 255.0
+    x_test = x_test.astype("float32") / 255.0
+
     return (x_train, y_train), (x_val, y_val)
