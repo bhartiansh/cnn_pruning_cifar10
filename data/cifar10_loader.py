5fc2a21f58385b419bfe3cc73d00ae241abc9cc9
bhartiansh <139680027+bhartiansh@users.noreply.github.com>
2025-04-16 12:56:40 +0530

    Update cifar10_loader.py

--- data/cifar10_loader.py
+++ data/cifar10_loader.py
@@ -1,18 +1,15 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 def load_cifar10_data(batch_size=64):
     (x_train, y_train), (x_val, y_val) = tf.keras.datasets.cifar10.load_data()
-    x_train, x_val = x_train / 255.0, x_val / 255.0
 
-    datagen = ImageDataGenerator(
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        horizontal_flip=True,
-    )
-    datagen.fit(x_train)
+    # Normalize
+    x_train = x_train.astype("float32") / 255.0
+    x_val = x_val.astype("float32") / 255.0
 
-    train_gen = datagen.flow(x_train, y_train, batch_size=batch_size)
-    val_gen = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size)
+    # Labels must be flat
+    y_train = y_train.flatten()
+    y_val = y_val.flatten()
 
-    return train_gen, val_gen
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(5000).batch(batch_size)
+    val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size)
+
+    return train_ds, val_ds
